7b4493c2e6963283ba921e8a5cc95cc7c5581aff
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-02 23:56:28 +0000

    Create home.jsx and homecard.jsx
    In this commit, I've finished the design and development of the Home section, ensuring a professional and engaging look that aligns with our project's visual identity. The section includes elements like layout, content, and responsive features to enhance user experience. This update enhances the overall aesthetics of our project and is ready for review and integration.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import HomeCard from './../component/HomeCard';
+import Filter from './../component/Filter';
 
 const Home = () => {
-  return (    
-    <div>
-      Home
+  return (
+    <div class='m-3'>
+      <Filter/>
+      <HomeCard/>
+      <HomeCard/>
+      <HomeCard/>
+      <HomeCard/>
+      <HomeCard/>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
