39fd93686b0b2a6458d8cfc1b516db86528bc216
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-02 19:13:25 +0000

    Complete Navbar Design
    In this commit, I've finished the design for the navbar, ensuring a professional and polished look that aligns with our project's visual identity. The design includes elements like layout, colors, and responsive features to enhance user experience and navigation. This update enhances the overall aesthetics of our project and is ready for review and integration.

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,24 +5,24 @@ import images from '../assets/images/images';
 const Navbar = () => {
   return (
     <div className='w-full flex items-center justify-between h-20 px-3'>
-      <div>
-        <icons.menu />
+      <div className='w-3/12 flex items-center soace-x-4'>
+        <icons.menu className='icon-style'/>
         <img className='w-32' src={images.youtube} alt="" />
       </div>
-      <div>
-        <div>
-          <input type="type" placeholder='Search' />
-          <div>
-            <icons.search />
+      <div className='w-6/12 flex items-center space-x-4'>
+        <div className='w-3/4 flex items-center'>
+          <input className='border p-2 h-10 flex-1' type="text" placeholder='Search' />
+          <div className='bg-gray-100 h-10 w-20 flex items-center justify-center border'>
+            <icons.search className='icon-style'/>
           </div>
         </div>
-        <div>
-          <icons.microphone />
+        <div className='w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center cursor-pointer'>
+          <icons.microphone className='icon-style'/>
         </div>
       </div>
-      <div>
-        <icons.video />
-        <icons.notification />
+      <div className='w-3/12 flex items-center justify-end space-x-5'>
+        <icons.video className='icon-style'/>
+        <icons.notification className='icon-style'/>
         <div className='w-10 h-10 bg-gray-200 rounded-full'></div>
       </div>
     </div>
