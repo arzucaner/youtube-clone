e6bc9b318ccb5e9c7a1a70d22ce8008ac9879bdf
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-02 13:29:09 +0000

    Add navbar, pages, icons, and images files
    This commit introduces several important enhancements to the project:
    
    - **Navbar**: A navigation bar has been added to the application, improving user navigation and providing quick access to different sections and features.
    
    - **Pages**: New pages have been created, or existing ones have been updated to enhance the functionality and content of the project, making it more feature-rich and user-friendly.
    
    - **Icons**: A set of icons has been incorporated to enhance the visual design and user interface, improving the overall look and feel of the application.
    
    - **Images**: Essential images have been added to the project for both decorative and informational purposes, contributing to a more engaging user experience.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Search from './pages/Search';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="videos/:id" element={<Detail />} />
+          <Route path="search/: keyword" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/assets/images/icons.js
@@ -0,0 +1,5 @@
+import { HiMicrophone } from 'react-icons/hi';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+import { BiLike } from 'react-icons/bi';
+import { BiDislike } from 'react-icons/bi';
