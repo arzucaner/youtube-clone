a506555a96cf4e1ac7e2894c07e29af2b8eb8937
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-03 23:05:14 +0000

    Update homecard.jsx

--- src/component/HomeCard.jsx
+++ src/component/HomeCard.jsx
@@ -3,8 +3,14 @@ import images from '../assets/images/images'
 
 const HomeCard = () => {
   return (
-    <div className='w-[300px]'>
+    <div className='w-[300px] m-3 cursor-pointer'>
       <img src={images.img} alt="" />
+      <div Classname='font-bold mt-1'>Title Title Title Title Title Title Title Title Title Title</div>
+      <div className='text-xs mt-1 tracking-wider text-gray-700'>Arzu Caner</div>
+      <div className='text-xs mt-1 tracking-wider text-gray-700'>
+        <span>300K views</span>
+        <span>3 years ago</span>
+      </div>
     </div>
   )
 }
