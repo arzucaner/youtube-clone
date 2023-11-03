2dd6a201df74967777d75324b7d561f1820aeb37
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-03 23:46:55 +0000

    Add filter.jsx and datafilter.js
    This commit adds two new files, "filter.jsx" and "datafilter.js," to the repository.
    
    - "filter.jsx" contains the React component for filtering data based on the values in "datafilter.js."
    - "datafilter.js" is a data source file that provides a list of filter options.
    
    These new files will enhance the filtering functionality of the project and improve data management.

---
+++ src/assets/data/dataFilter.js
@@ -0,0 +1,18 @@
+const dataFilter = [
+    { id: 0, name: "All" },
+    { id: 1, name: "Mix's" },
+    { id: 2, name: "Music" },
+    { id: 3, name: "Live" },
+    { id: 4, name: "Fitness" },
+    { id: 5, name: "Game" },
+    { id: 6, name: "Comedy skits" },
+    { id: 7, name: "Mathematics" },
+    { id: 8, name: "Football" },
+    { id: 9, name: "Rap" },
+    { id: 10, name: "Cooking" },
+    { id: 11, name: "Recent uploads" }
+];
+
+export default dataFilter;
+
+
--- src/component/Filter.jsx
+++ src/component/Filter.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import dataFilter from '../assets/data/dataFilter';
 
 const Filter = () => {
   return (
-    <div>
-      Filter
+    <div className='Flex items-center'>
+      {
+        dataFilter.map((data, i) => (
+          <div className='bg-gray-100 cursor-pointer hover:bg-gray-200 px-3 py-1 mx-2 rounded-2xl' key={i}>
+            {data.name}
+          </div>
+        ))
+
+      }
     </div>
   );
-}
+};
 
 export default Filter;
