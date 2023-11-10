3b7b732a81bc33416e5877e89e2df39ea03df330
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-10 23:38:56 +0000

    Update searchcard.jsx

--- src/component/SearchCard.jsx
+++ src/component/SearchCard.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import images from '../assets/images/images';
 
-const SearchCard = () {
+const SearchCard = () => {
     return (
-        <div className='flex space-x-2'>
+        <div className='flex space-x-2 justify-center m-2'>
             <img src={images.img} alt="" />
-            <div>
-                <div className='font-bold text-xl'></div>
+            <div className='w-2/3 space-y-2'>
+                <div>
+                    <div className='font-bold text-lg'>It is a long established fact that a reader will be distracted by the readable content of
+                        a page when looking at its layout.
+                        <div>
+                            <span>4,5 Views *</span>
+                            <span>5 hours ago</span>
+                        </div>
+                        <div className='flex items-center space-x-2'>
+                            <div className="w-10 h-10 rounded-full bg-gray-200"></div>
+                            <div className="text-gray-700">BOOKS</div>
+                        </div>
+                        <div>
+                            It is a long established fact that a reader will be distracted by the readable content of
+                            a page when looking at its layout.</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
