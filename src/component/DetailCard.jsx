477bb14aa3d018de4f0e72139f772e68caf7b52f
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-10 21:39:25 +0000

    Update detailcard.jsx

--- src/component/DetailCard.jsx
+++ src/component/DetailCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
+import moment from 'moment';
 
-const DetailCard = ({video}) => {
+const DetailCard = ({ video }) => {
   return (
     <div>
-      DetailCard
+      <ReactPlayer url={`https://youtube.com/watch?v=${video?.id}`} />
+      <div className='font-bold text-2xl mt-1'>{video?.snippet?.title}</div>
+      <div className='space-x-2 text-gray-800 mt-1'></div>
+      <div>
+        <span>
+          {video?.statistics?.viewCount} Views
+        </span>
+        <span>{moment(video?.snippet?.publishedAt).startOf('day').fromNow()}
+        </span>
+      </div>
     </div>
   )
 }
