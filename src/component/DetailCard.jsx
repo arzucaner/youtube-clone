30c4126452f4830e0afa88a13b76485d58caaf4e
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-10 22:06:50 +0000

    Update detailcard.jsx

--- src/component/DetailCard.jsx
+++ src/component/DetailCard.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import moment from 'moment';
+import icons from '../assets/icons';
 
 const DetailCard = ({ video }) => {
   return (
-    <div>
+    <div className='max-w-[650px] m-10'>
       <ReactPlayer url={`https://youtube.com/watch?v=${video?.id}`} />
       <div className='font-bold text-2xl mt-1'>{video?.snippet?.title}</div>
-      <div className='space-x-2 text-gray-800 mt-1'></div>
-      <div>
+      <div className='space-x-2 text-gray-800 mt-1'>
         <span>
           {video?.statistics?.viewCount} Views
         </span>
         <span>{moment(video?.snippet?.publishedAt).startOf('day').fromNow()}
         </span>
+      </div>
+      <div className='text-gray-700 mt-2 text-sm'>
+        {(video?.snippet?.description).substring(0, 200)}...
+      </div>
+      <div className='mt-2 text-xs text-blue-700'>
+        {
+          video?.snippet?.tags?.map((tag, i) => `#${tag} `)
+        }
+      </div>
+      <div>
+        <span><icons.like/> {video?.statistics?.likeCount}</span>
       </div>
     </div>
   )
