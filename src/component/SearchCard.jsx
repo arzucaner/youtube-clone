d583722f5fd48b0359b77b9d60bce902ecfb18fe
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-11 06:46:11 +0000

    Add searchdata.js

--- src/component/SearchCard.jsx
+++ src/component/SearchCard.jsx
@@ -3,21 +3,21 @@ import images from '../assets/images/images';
 
 const SearchCard = () => {
     return (
-        <div className='flex space-x-2 justify-center m-2'>
+        <div className='flex space-x-2 justify-center mx-5 my-10 cursor-pointer'>
             <img src={images.img} alt="" />
             <div className='w-2/3 space-y-2'>
                 <div>
-                    <div className='font-bold text-lg'>It is a long established fact that a reader will be distracted by the readable content of
-                        a page when looking at its layout.
-                        <div>
+                    <div className='font-bold text-lg'>It is a long established fact that a reader will be distracted by the readable 
+                     content of a page when looking at its layout.
+                        <div className='text-sm text-gray-700'>
                             <span>4,5 Views *</span>
                             <span>5 hours ago</span>
                         </div>
-                        <div className='flex items-center space-x-2'>
+                        <div className='flex items-center space-x-2 text-sm'>
                             <div className="w-10 h-10 rounded-full bg-gray-200"></div>
                             <div className="text-gray-700">BOOKS</div>
                         </div>
-                        <div>
+                        <div className='text-sm text-gray=700'>
                             It is a long established fact that a reader will be distracted by the readable content of
                             a page when looking at its layout.</div>
                     </div>
---
+++ src/redux/actions/SearchData.js
@@ -0,0 +1,37 @@
+
+import axios from 'axios';
+require('dotenv').config();
+
+const apiKey = process.env.YOUR_API_KEY;
+
+const searchAction = (keyword) => async (dispatch) => {
+    try {
+        const options = {
+            method: 'GET',
+            url: 'https://youtube-v31.p.rapidapi.com/search',
+            params: {
+                q: keyword,
+                part: 'snippet,id',
+                regionCode: 'US',
+                maxresults: '50',
+                order: 'date'
+            },
+
+            headers: {
+                'X-RapidAPI-Key': apiKey,
+                'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            dispatch({ type: 'GET_Search', payload: response.data })
+        } catch (error) {
+            console.error(error);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default searchAction
