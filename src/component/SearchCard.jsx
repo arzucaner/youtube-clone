960551e51f84d8eac125a3f8808d947b107eb61c
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-11 08:10:34 +0000

    Update searchcard.jsx

--- src/component/SearchCard.jsx
+++ src/component/SearchCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import images from '../assets/images/images';
+import moment from 'moment';
 
 const SearchCard = ({ search }) => {
     return (
@@ -7,24 +8,24 @@ const SearchCard = ({ search }) => {
             <img className='h-44 w-72' src={search?.snippet?.thumbnails?.high?.url} alt="" />
             <div className='w-2/3 space-y-2'>
                 <div>
-                    <div className='font-bold text-lg'>It is a long established fact that a reader will be distracted by the readable
-                        content of a page when looking at its layout.
-                        <div className='text-sm text-gray-700'>
-                            <span>4,5 Views *</span>
-                            <span>5 hours ago</span>
-                        </div>
-                        <div className='flex items-center space-x-2 text-sm'>
-                            <div className="w-10 h-10 rounded-full bg-gray-200"></div>
-                            <div className="text-gray-700">BOOKS</div>
-                        </div>
-                        <div className='text-sm text-gray=700'>
-                            It is a long established fact that a reader will be distracted by the readable content of
-                            a page when looking at its layout.</div>
+                    <div className='font-bold text-lg'>{search?.snippet?.title}</div>
+                    <div className='text-sm text-gray-700'>
+                        <span>4,5 Views *</span>
+                        <span>5 hours ago</span>
+                        <span>{moment(search?.snippet?.publishAt).startOf('day').fromNow()}</span>
+                    </div>
+                    <div className='flex items-center space-x-2 text-sm'>
+                        <div className="w-10 h-10 rounded-full bg-gray-200"></div>
+                        <div className="text-gray-700">BOOKS</div>
+                    </div>
+                    <div className='text-sm text-gray=700'>
+                        {search?.snippet?.description ? search?.snippet?.description : "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
+
 
 export default SearchCard;
